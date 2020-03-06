754536590d2edb60d1f0436b90c930dab2cb82f6
bulatko <buloch03@yandex.ru>
2020-03-06 11:48:02 +0300

    code format

--- 01/allocator.cpp
+++ 01/allocator.cpp
@@ -7,21 +7,24 @@ int last = 0, block_size;
 
 void makeAllocator(int num){
 
-block = (char*)malloc(num);
-block_size = num;
+    block = (char*)malloc(num);
+    block_size = num;
 }
 
 char* alloc(int num){
 
-int t = last;
-if(last + num > block_size)
-    return nullptr;
-last += num;
-return block + t;
+    int t = last;
+
+    if(last + num > block_size)
+        return nullptr;
+
+    last += num;
+
+    return block + t;
 }
 
 void reset(){
 
-last = 0;
+    last = 0;
 
 }
